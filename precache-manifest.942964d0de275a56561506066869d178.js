da71db4fea1024ddca5180ba55aacd63bc72e5a3
1ung <leewhe@hotmail.com>
2020-07-16 00:12:57 +0800

    Updates

---
+++ precache-manifest.942964d0de275a56561506066869d178.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970555a0270cc40b57a0efb67f95ffb6",
+    "url": "/naga-capital/index.html"
+  },
+  {
+    "revision": "6e9ecaa4e1864b66976b",
+    "url": "/naga-capital/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1d6a55e3d40552cce742",
+    "url": "/naga-capital/static/css/main.81c40363.chunk.css"
+  },
+  {
+    "revision": "6e9ecaa4e1864b66976b",
+    "url": "/naga-capital/static/js/2.e030bd34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/naga-capital/static/js/2.e030bd34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d6a55e3d40552cce742",
+    "url": "/naga-capital/static/js/main.dd44f4a0.chunk.js"
+  },
+  {
+    "revision": "ffe11d6e11251f69a54d",
+    "url": "/naga-capital/static/js/runtime-main.7badf974.js"
+  },
+  {
+    "revision": "77e5bdbda3c8180fc6f5305d7a555bc5",
+    "url": "/naga-capital/static/media/home-bg.77e5bdbd.jpg"
+  },
+  {
+    "revision": "3cc50162c64089d26e8f89f52be74985",
+    "url": "/naga-capital/static/media/sample-icon.3cc50162.png"
+  },
+  {
+    "revision": "5cdc477e28fd21fdb1f32b413b341f20",
+    "url": "/naga-capital/static/media/sg-bg.5cdc477e.jpg"
+  }
+]);
